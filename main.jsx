1e800da03a087563ba8ccebb860e83b15d36dbe3
NIRVIDATE <nirbsfc@gmail.com>
2025-06-18 21:56:04 +0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,9 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './Home.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://cbehmvmhatbvqrtipprs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiZWhtdm1oYXRidnFydGlwcHJzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDI2NTc5OSwiZXhwIjoyMDY1ODQxNzk5fQ.WhSvPIGsWk-s3iaFGs0aeLBd_WEjFD1q5FmwzQCjnoI';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = {}  // placeholder
